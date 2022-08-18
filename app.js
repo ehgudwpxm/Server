761e926d681e79e5e72296d9c02dc0bdcb3e6660
AIS-NB-103\AIS-N <xixxaxon@gmail.com>
2022-08-18 15:46:59 +0900

    33333

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 
-const dfInfo = require('../DB/dbinfo')
+//const dfInfo = require('../DB/dbinfo')
 
 
 const { Client } = require('pg') //DB사용을 위함
